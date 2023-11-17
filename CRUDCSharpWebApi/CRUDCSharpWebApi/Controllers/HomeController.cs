65c0f0b03801947d0be5fce265ea3b04b254f29e
MayconL27 <mayconleandro2008@gmail.com>
2023-11-17 08:39:33 -0300

    New projeto Events

---
+++ CRUDCSharpWebApi/CRUDCSharpWebApi/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDCSharpWebApi.Controllers
+{
+    [Route("api/controller")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+    
+    }
+}
---
+++ CRUDCSharpWebApi/CRUDCSharpWebApi/Entities/Event.cs
@@ -0,0 +1,31 @@
+﻿namespace CRUDCSharpWebApi.Entities
+{
+    public class Event
+    {
+        public Event() 
+        {
+            Speakers = new List<EventSpeaker>();
+            IsDeleted = false;
+        }
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public List<EventSpeaker> Speakers { get; set; }
+        public bool IsDeleted { get; set; }
+
+        public void Update(string title, string description, DateTime startDate, DateTime endDate)
+        {
+            Title = title;
+            Description = description;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public void Delete() 
+        { 
+            IsDeleted = true;
+        }
+    }
+}
